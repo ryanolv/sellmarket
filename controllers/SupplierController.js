4c814595dfb9098a8d6f99fc77e21531e353995e
Ryan da Silva Oliveira <ryanprofissional02@gmail.com>
2023-05-09 00:23:39 -0300

    Fazendo validação de dados do fornecedor no SupplierController

--- controllers/SupplierController.js
+++ controllers/SupplierController.js
@@ -4,8 +4,21 @@ module.exports = class SupplierController {
 
     static async register(request, response) {
 
-        response.json({ "message": "hi"})
+        const { email_supplier, name_supplier, cnpj_supplier} = request.body;
 
+        // Validating datas
+        if(!email_supplier) {
+            response.status(420).json({ "message": "Campo de email do fornecedor inválido!" });
+            return
+        }
+        if(!name_supplier) {
+            response.status(420).json({ "message": "Campo de nome do fornecedor inválido!" });
+            return
+        }
+        if(!cnpj_supplier) {
+            response.status(420).json({ "message": "Campo de CNPJ do fornecedor inválido!" });
+            return
+        }
     }
 
 }
